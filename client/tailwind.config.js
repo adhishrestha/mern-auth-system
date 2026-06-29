96821f3cd21e10ede44f7d5027cd031d630134e6
adhishrestha <aditya16stha@gmail.com>
2026-06-29 17:16:13 +0545

    chore(client): initialize React application and project Strucutre

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      "2xl": "1440px",
+    },
+  },
+  plugins: [],
+};
