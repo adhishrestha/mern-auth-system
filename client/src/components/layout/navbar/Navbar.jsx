ef67e3e0461e04b91676e30198c46e784e0a443b
adhishrestha <aditya16stha@gmail.com>
2026-07-05 21:35:58 +0545

    feat(navbar): hide navbar on scroll and lock body scroll for mobile menu

--- client/src/components/layout/navbar/Navbar.jsx
+++ client/src/components/layout/navbar/Navbar.jsx
@@ -4,20 +4,31 @@ import DesktopNav from './DesktopNav';
 import MobileNav from './MobileNav';
 import MobileSidebar from './MobileSidebar';
 import useEscapeKey from '../../../hooks/useEscapeKey';
+import useBodyScrollLock from '@/hooks/useBodyScrollLock';
+import useScrollDirection from '@/hooks/useScrollDirection';
 
 const Navbar = () => {
   const [openMenu, setOpenMenu] = useState(false);
   const [showUserMenu, setShowUserMenu] = useState(false);
+
+  useBodyScrollLock(openMenu);
 
   useEscapeKey(() => {
     setOpenMenu(false);
     setShowUserMenu(false);
   });
 
+  const showNavbar = useScrollDirection();
+  useEffect(() => {
+    if (!showNavbar) {
+      setShowUserMenu(false);
+    }
+  }, [showNavbar]);
+
   return (
     <>
       <header
-        className={`fixed top-0 left-0 z-40 w-full bg-white shadow-sm transition-transform duration-300`}
+        className={`fixed top-0 left-0 z-40 w-full bg-white shadow-sm transition-transform duration-300 ${showNavbar ? 'translate-y-0' : '-translate-y-full'}`}
       >
         <Container>
           <nav aria-label="Main navigation">
