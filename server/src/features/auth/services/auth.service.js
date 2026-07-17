235d2629820aec203376525b146609635af3070c
adhishrestha <aditya16stha@gmail.com>
2026-07-17 23:53:30 +0545

    feat(auth): hash user passwords during registration

--- server/src/features/auth/services/auth.service.js
+++ server/src/features/auth/services/auth.service.js
@@ -1,5 +1,8 @@
+import bcrypt from "bcrypt";
 import ApiError from "../../../utils/ApiError.js";
 import User from "../models/user.model.js";
+
+const SALT_ROUNDS = 12;
 
 const registerUser = async ({ fullName, email, password }) => {
   const existingUser = await User.findOne({ email });
@@ -8,10 +11,12 @@ const registerUser = async ({ fullName, email, password }) => {
     throw new ApiError(409, "Email already registered");
   }
 
+  const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+
   const user = await User.create({
     fullName,
     email,
-    password,
+    password: hashedPassword,
   });
 
   return user;
