cdcfddb880aca0fb524a0eb479868e7a96b3bac1
adhishrestha <aditya16stha@gmail.com>
2026-07-25 10:25:55 +0545

    feat: configure environment variables with dotenv

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,10 @@
+import dotenv from "dotenv";
 import app from "./app.js";
 
-const PORT = 5000;
+//Load environment variables
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
